964f37a1224428ae472af88f0aa4b9beb29fd754
Bug <77031032+0zBug@users.noreply.github.com>
2023-07-24 23:17:46 -0500

    Create R6.lua

---
+++ Assets/Scripts/R6.lua
@@ -0,0 +1,66 @@
+return {
+    Idle = {
+        Animation1 = 12521158637,
+        Animation2 = 12521162526
+    },
+    Run = {
+        RunAnim = 12518152696
+    },
+    Walk = {
+        WalkAnim = 12518152696
+    },
+    Jump = {
+    	JumpAnim = 12520880485
+    },
+    Fall = {
+        FallAnim = 12520972571
+    },
+    Climb = {
+    	ClimbAnim = 12520982150
+    },
+    Dance = {
+    	Animation1 = 12521009666,
+    	Animation2 = 12521151637,
+    	Animation3 = 12521015053
+    },
+    Dance2 = {
+    	Animation1 = 12521169800,
+    	Animation2 = 12521173533,
+    	Animation3 = 12521027874
+    },
+    Dance3 = {
+    	Animation1 = 12521178362,
+    	Animation2 = 12521181508,
+    	Animation3 = 12521184133
+    },
+    Laugh = {
+    	LaughAnim = 12521018724
+    },
+    Cheer = {
+    	CheerAnim = 12521021991
+    },
+    Sit = {
+    	SitAnim = 12520993168
+    },
+    Wave = {
+    	WaveAnim = 12521004586
+    },
+    Point = {
+    	PointAnim = 12521007694
+    },
+    ToolNone = {
+    	ToolNoneAnim = 12520996634
+    },
+    ToolSlash = {
+    	ToolSlashAnim = 12520999032
+    },
+    ToolLunge = {
+    	ToolLungeAnim = 12521002003
+    },
+    Swim = {
+    	Swim = 12518152696
+    },
+    SwimIdle = {
+    	SwimIdle = 12518152696
+    }
+}
